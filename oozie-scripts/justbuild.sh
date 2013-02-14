56cb9494000a2511c83a5c284c720d7eac6c8744
Lee T. Tucker <ltucker@verticloud.com>
2013-02-14 14:16:41 -0800

    Align with built artifacts.

--- oozie-scripts/justbuild.sh
+++ oozie-scripts/justbuild.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -ex
+umask 0022
+export MVN_LOCAL="-Dmaven.repo.local=${WORKSPACE}/.m2 --settings=${WORKSPACE}/.m2/settings.xml"
 mvn versions:set -DnewVersion=${ARTIFACT_VERSION}
-oozie/bin/mkdistro.sh
+# hack around the lack of extensibility in the mkdistro script
+sed -e s/assembly\:single/assembly\:single\ \$\{MVN_LOCAL\}/ < bin/mkdistro.sh > ${WORKSPACE}/oozie-scripts/mkdistro.sh
+/bin/sh -ex ${WORKSPACE}/oozie-scripts/mkdistro.sh
 
--- oozie-scripts/justinstall.sh
+++ oozie-scripts/justinstall.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-mkdir -p --mode=0755 ${INSTALL_DIR}/opt
-cd ${INSTALL_DIR}/opt
-####tar -xvzpf ${WORKSPACE}/hcatalog/build/hcatalog-${ARTIFACT_VERSION}.tar.gz
+export ${DEST_DIR}=${INSTALL_DIR}/opt
+mkdir -p --mode=0755 ${DEST_DIR}
+cd ${DEST_DIR}
+tar -xvzpf ${WORKSPACE}/oozie/distro/target/oozie-${ARTIFACT_VERSION}-distro/oozie-${ARTIFACT_VERSION}/oozie-client-${ARTIFACT_VERSION}.tar.gz
 
-export RPM_NAME=vcc-oozie-${ARTIFACT_VERSION}
+# Make the Client RPM
+
+export RPM_NAME=vcc-oozie-client-${ARTIFACT_VERSION}
 export RPM_VERSION=0.1.0
 
 cd ${RPM_DIR}
@@ -23,3 +26,30 @@ fpm --verbose \
 --rpm-group root \
 -C ${INSTALL_DIR} \
 opt
+
+# Make the Server RPM
+
+rm -rf ${DEST_DIR}
+mkdir -p --mode=0755 ${DEST_DIR}
+tar -xvzpf ${WORKSPACE}/oozie/distro/target/oozie-${ARTIFACT_VERSION}-distro.tar.gz
+
+export RPM_NAME=vcc-oozie-server-${ARTIFACT_VERSION}
+export RPM_VERSION=0.1.0
+
+cd ${RPM_DIR}
+
+fpm --verbose \
+--maintainer ops@verticloud.com \
+--vendor VertiCloud \
+--provides ${RPM_NAME} \
+-s dir \
+-t rpm \
+-n ${RPM_NAME} \
+-v ${RPM_VERSION} \
+--description "${DESCRIPTION}" \
+--iteration ${DATE_STRING} \
+--rpm-user root \
+--rpm-group root \
+-C ${INSTALL_DIR} \
+opt
+
