9d79ea7bae287172526cd61885a40d1bf0e5e137
Lee T. Tucker <ltucker@verticloud.com>
2013-02-14 14:34:51 -0800

    Have to be in the oozie dir to do the build

--- oozie-scripts/justbuild.sh
+++ oozie-scripts/justbuild.sh
@@ -4,5 +4,6 @@ export MVN_LOCAL="-Dmaven.repo.local=${WORKSPACE}/.m2 --settings=${WORKSPACE}/.m
 mvn versions:set -DnewVersion=${ARTIFACT_VERSION}
 # hack around the lack of extensibility in the mkdistro script
 sed -e s/assembly\:single/assembly\:single\ \$\{MVN_LOCAL\}/ < bin/mkdistro.sh > ${WORKSPACE}/oozie-scripts/mkdistro.sh
+cd ${WORKSPACE}/oozie
 /bin/sh -ex ${WORKSPACE}/oozie-scripts/mkdistro.sh
 
