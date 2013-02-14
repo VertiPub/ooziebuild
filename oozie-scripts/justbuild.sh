c9466ca200936697179244a96b35f71174bde2d9
Lee T. Tucker <ltucker@verticloud.com>
2013-02-14 15:20:18 -0800

    local m2 repo

--- oozie-scripts/justbuild.sh
+++ oozie-scripts/justbuild.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -ex
 umask 0022
 cd ${WORKSPACE}/oozie
+mkdir ${WORKSPACE}/.m2
+echo "<settings></settings>" > ${WORKSPACE}/.m2/settings.xml
 export MVN_LOCAL="-Dmaven.repo.local=${WORKSPACE}/.m2 --settings=${WORKSPACE}/.m2/settings.xml -DskipTests"
 mvn versions:set -DnewVersion=${ARTIFACT_VERSION} ${MVN_LOCAL}
 # hack around the lack of extensibility in the mkdistro script
