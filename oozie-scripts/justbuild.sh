8881d630d9fbc3303e0d6702ba4f79fcc8ede7d6
Lee T. Tucker <ltucker@verticloud.com>
2013-02-14 18:36:50 -0800

    Update to point to 2.0.2-alpha hadoop.

--- oozie-scripts/justbuild.sh
+++ oozie-scripts/justbuild.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -ex
 umask 0022
 cd ${WORKSPACE}/oozie
-mkdir ${WORKSPACE}/.m2
+mkdir -p ${WORKSPACE}/.m2
 echo "<settings></settings>" > ${WORKSPACE}/.m2/settings.xml
-export MVN_LOCAL="-Dmaven.repo.local=${WORKSPACE}/.m2 --settings=${WORKSPACE}/.m2/settings.xml -DskipTests"
+export MVN_LOCAL="-Dmaven.repo.local=${WORKSPACE}/.m2 --settings=${WORKSPACE}/.m2/settings.xml -DskipTests -Dhadoop.version=2.0.2-alpha"
 mvn versions:set -DnewVersion=${ARTIFACT_VERSION} ${MVN_LOCAL}
 # hack around the lack of extensibility in the mkdistro script
 sed -e s/assembly\:single/assembly\:single\ \$\{MVN_LOCAL\}/ < bin/mkdistro.sh > ${WORKSPACE}/oozie-scripts/mkdistro.sh
