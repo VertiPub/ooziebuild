aa06f5e30133dbf3cafcdc7fe13672263cea3b26
Lee T. Tucker <ltucker@verticloud.com>
2013-08-14 16:03:53 -0700

    handle the link to hadoop conf

--- oozie-scripts/justinstall.sh
+++ oozie-scripts/justinstall.sh
@@ -35,17 +35,20 @@ rm -rf ${DEST_DIR}
 mkdir -p --mode=0755 ${DEST_DIR}
 cd ${DEST_DIR}
 tar -xvzpf ${WORKSPACE}/oozie/distro/target/oozie-${ARTIFACT_VERSION}-distro.tar.gz
-export OPT_ROOT=${DEST_DIR}/oozie-${ARTIFACT_VERSION}
-mkdir -p -m 0775 ${OPT_ROOT}/libext
-cd ${OPT_ROOT}/libext
+export OOZIE_ROOT=${DEST_DIR}/oozie-${ARTIFACT_VERSION}
+mkdir -p -m 0775 ${OOZIE_ROOT}/libext
+cd ${OOZIE_ROOT}/libext
 wget http://s3-us-west-1.amazonaws.com/verticloud-dependencies/ext-2.2.zip
 EXTSUM=`sha1sum ext-2.2.zip | awk '{print $1}'`
 if [ "${EXTSUM}" != "a949ddf3528bc7013b21b13922cc516955a70c1b" ]; then
   echo "FATAL: Filed to fetch the correct ext-2.2.zip"
 fi
-cd ${OPT_ROOT}
+cd ${OOZIE_ROOT}
 cp libtools/hadoop-* libext
 cp libtools/commons-cli-* libext
+cd ${OOZIE_ROOT}/conf
+rm -rf hadoop-conf
+ln -s /etc/hadoop hadoop-conf
 
 export RPM_NAME=vcc-oozie-server-${ARTIFACT_VERSION}
 export RPM_VERSION=0.2.0
