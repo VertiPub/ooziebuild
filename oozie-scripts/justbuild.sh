07579a31a3d32d5f6de69bb30b4695914070115b
Jenkins <jenkins@ip-10-253-16-167.us-west-2.compute.internal>
2013-09-12 20:21:13 +0000

    change the variable name so I can patch the poms to use it

--- oozie-scripts/justbuild.sh
+++ oozie-scripts/justbuild.sh
@@ -3,7 +3,7 @@ umask 0022
 cd ${WORKSPACE}/oozie
 mkdir -p ${WORKSPACE}/.m2
 echo "<settings></settings>" > ${WORKSPACE}/.m2/settings.xml
-export MVN_LOCAL="-Dmaven.repo.local=${WORKSPACE}/.m2 --settings=${WORKSPACE}/.m2/settings.xml -DskipTests -Dhadoop.version=2.0.5-alpha"
+export MVN_LOCAL="-Dmaven.repo.local=${WORKSPACE}/.m2 --settings=${WORKSPACE}/.m2/settings.xml -DskipTests -Dhadoop.two.version=2.0.5-alpha"
 mvn versions:set -DnewVersion=${ARTIFACT_VERSION} ${MVN_LOCAL}
 # hack around the lack of extensibility in the mkdistro script
 sed -e s/assembly\:single/assembly\:single\ \$\{MVN_LOCAL\}/ < bin/mkdistro.sh > ${WORKSPACE}/oozie-scripts/mkdistro.sh
