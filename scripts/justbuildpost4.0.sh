326b460f2da51eaa088e25adc93b1ea422ba5277
jblatt-verticloud <jblatt@verticloud.com>
2014-02-04 14:39:19 -0800

    DP-317 system property overrides for maven pom doesn't work for transitive dependencies.

--- scripts/justbuildpost4.0.sh
+++ scripts/justbuildpost4.0.sh
@@ -1,13 +1,19 @@
 #!/bin/sh -ex
+
+if [ -z ${WORKSPACE} ]; then
+   echo "WORKSPACE must be explicitly set in the environment"
+   exit 1
+fi
+if [ -n ${HADOOP_VERSION} || -n ${PIG_VERSION} || -n ${HIVE_VERSION} ]; then
+   echo "HADOOP_VERSION, PIG_VERSION, HIVE_VERSION are no longer supported.   You must modify the top-level pom.xml instead"
+   exit 1
+fi
+
 umask 0022
 cd ${WORKSPACE}/oozie
 mkdir -p ${WORKSPACE}/.m2
 echo "<settings></settings>" > ${WORKSPACE}/.m2/settings.xml
-if [ -z ${WORKSPACE} ] || [ -z ${HADOOP_VERSION} ] || [ -z ${PIG_VERSION} ] || [ -z ${HIVE_VERSION} ]; then
-   echo "HADOOP_VERSION, PIG_VERSION, HIVE_VERSION, and WORKSPACE must be explicitly set in the environment"
-   exit 1
-fi
-export MVN_LOCAL="-Dmaven.repo.local=${WORKSPACE}/.m2 --settings=${WORKSPACE}/.m2/settings.xml -DskipTests -P hadoop-2 -Dhadoop.version=${HADOOP_VERSION} -Dpig.version=${PIG_VERSION} -Dhive.version=${HIVE_VERSION} -Dhadoop.auth.version=${HADOOP_VERSION}"
+export MVN_LOCAL="-Dmaven.repo.local=${WORKSPACE}/.m2 --settings=${WORKSPACE}/.m2/settings.xml -DskipTests -P hadoop-2"
 mvn versions:set -DnewVersion=${ARTIFACT_VERSION} ${MVN_LOCAL}
 # hack around the lack of extensibility in the mkdistro script
 sed -e s/assembly\:single/assembly\:single\ \$\{MVN_LOCAL\}/ < bin/mkdistro.sh > ${WORKSPACE}/scripts/mkdistro.sh
