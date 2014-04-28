f29bafda1a5b5d5d5c6addf48e56438e3a545674
Ravi Prakash <raviprak@apache.org>
2014-04-28 09:18:55 -0700

    Setting .m2 again

--- scripts/justbuildpost4.0.sh
+++ scripts/justbuildpost4.0.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -ex
 #umask 0022
 #cd ${WORKSPACE}/oozie
-#mkdir -p ${WORKSPACE}/.m2
-#echo "<settings></settings>" > ${WORKSPACE}/.m2/settings.xml
+mkdir -p ${WORKSPACE}/.m2
+echo "<settings></settings>" > ${WORKSPACE}/.m2/settings.xml
 #if [ -z ${WORKSPACE} ] || [ -z ${HADOOP_VERSION} ] || [ -z ${PIG_VERSION} ] || [ -z ${HIVE_VERSION} ]; then
 #   echo "HADOOP_VERSION, PIG_VERSION, HIVE_VERSION, and WORKSPACE must be explicitly set in the environment"
 #   exit 1
@@ -13,4 +13,4 @@
 #sed -e s/assembly\:single/assembly\:single\ \$\{MVN_LOCAL\}/ < bin/mkdistro.sh > ${WORKSPACE}/scripts/mkdistro.sh
 #mv ${WORKSPACE}/scripts/mkdistro.sh bin/mkdistro.sh
 #/bin/sh -ex bin/mkdistro.sh
-mvn package assembly:single -DskipTests=true -Phadoop-2
+mvn package assembly:single -Dmaven.repo.local=${WORKSPACE}/.m2 --settings=${WORKSPACE}/.m2/settings.xml -DskipTests=true -Phadoop-2
